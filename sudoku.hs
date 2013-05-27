79cab749dcf341353f864df21cc91fa7c5409c8d
inaimathi <inaimathi@lambda>
2013-05-27 08:41:16 -0400

    -Board class (rather than just 2D array)

--- sudoku.hs
+++ sudoku.hs
@@ -1,80 +1,100 @@
 module Sudoku where
 
-import Data.Set (Set(..), toList, fromList, union, difference)
-import Data.List (sortBy)
+import Data.Set (Set(..), toList, fromList, union, difference, intersection)
+import qualified Data.Set as Set
+import Data.List (sortBy, intercalate)
+import Data.List.Split (chunksOf)
 import Data.Ord (comparing)
+import Data.Char (intToDigit)
 
-type Board = [[Int]]
+data Board = Board { values :: [[Int]], 
+                     empty :: Set (Int, Int),
+                     size :: Int, 
+                     ixs :: [Int],
+                     blockSize :: Int } deriving (Eq)
+
+instance Show Board where
+  show board = unlines . intercalate hdelim . split . lns $ values board
+    where lns = map (intercalate "|" . split . map sq)
+          split = chunksOf bs
+          sq n = if n == 0 then ' ' else intToDigit n
+          hdelim = [replicate (size board + (bs - 1)) '-']
+          bs = blockSize board
 
 row :: Board -> (Int, Int) -> Set Int
-row board (x, y) = fromList $ board !! y
+row board (x, y) = fromList $ values board !! y
 
 col :: Board -> (Int, Int) -> Set Int
-col board (x, y) = fromList $ map (!! x) board
+col board (x, y) = fromList . map (!! x) $ values board
 
 block :: Board -> (Int, Int) -> Set Int
-block board (x, y) = fromList . concat . map (take blockSize . drop ox) $ take blockSize $ drop oy board
-  where blockSize = fromEnum . sqrt . toEnum $ length board
-        origin n = blockSize * intFloor n blockSize
-        ox = origin x
-        oy = origin y
+block board (x, y) = fromList . concat . square $ values board
+  where square = map (take bs . drop (origin x)) . take bs . drop (origin y)
+        origin n = bs * intFloor n bs
+        bs = blockSize board
+
+blockEmpties :: Board -> (Int, Int) -> [(Int, Int)]
+blockEmpties board (x, y) = [(x', y') | x' <- xs, y' <- ys, blank (x', y')]
+  where blank (x, y) = 0 == ((values board) !! y !! x)
+        xs = [ox..ox + bs]
+        ys = [oy..oy + bs]
+        [ox, oy] = map origin [x, y]
+        origin n = bs * intFloor n bs
+        bs = (blockSize board)-1
 
 possibilities :: Board -> (Int, Int) -> Set Int
-possibilities board (x, y) = foldl difference (fromList [1..9]) sets
+possibilities board (x, y) = foldl difference (fromList [1..size board]) sets
   where sets = mapply (board, (x, y)) [row, col, block]
 
-empties :: Board -> Bool
-empties board = any id $ map (any (== 0)) board
-
 solve :: Board -> Board
-solve board = case (obvious == board, empties obvious) of
-  (True, False) -> board
-  (True, True) -> obvious -- call guess here
-  (False, _) -> solve obvious
-  where ixs = [0..(length board - 1)]
-        obvious = [map (\x -> newVal (x, y)) ixs | y <- ixs]
-        newVal (x, y) = case (board !! y !! x, toList $ possibilities board (x, y)) of
+solve board = case (new == board, Set.size $ empty new) of
+  (_, 0) -> new
+  (True, _) -> new -- next stage
+  (False, _) -> solve new
+  where new = obvious board
+
+obvious board = findEmpties $ board { values = newVals }
+  where newVals = [map (\x -> newVal (x, y)) $ ixs board | y <- ixs board]
+        ps x y = toList $ possibilities board (x, y)
+        newVal (x, y) = case ((values board) !! y !! x, ps x y) of
           (0, [val]) -> val
           (val, _) -> val
 
-guess board = sortBy (comparing pLen) [(x, y, toList $ possibilities board (x, y)) | x <- ixs, y <- ixs, 0 == (board !! y !! x)]
-  where ixs = [0..(length board - 1)]
-        pLen (x, y, p) = length p
+-- neededInBlock board (x, y) = difference (fromList [1..size board]) $ block board (x, y)
+-- blockPos board (x, y) = map (possibilities board) $ blockEmpties board (x, y)
+-- uniqueInBlock board (x, y) = foldl intersection $ block board (x, y):ps
+--   where ps = map (possibilities board) $ blockEmpties board (x, y)
+-- blockWise board = findEmpties $ board { values = newVals }
+--   where 
 
----------- Utility and sample data
-sample :: Board
-sample = [[0, 7, 1, 4, 0, 0, 0, 0, 5],
-          [0, 0, 0, 0, 5, 0, 0, 8, 0],
-          [0, 0, 3, 9, 0, 7, 6, 0, 0],
-          [0, 0, 0, 0, 0, 1, 0, 0, 0],
-          [0, 9, 0, 8, 0, 6, 0, 0, 3],
-          [0, 0, 0, 0, 0, 0, 8, 2, 0],
-          [0, 6, 0, 0, 4, 0, 7, 0, 8],
-          [3, 0, 0, 0, 0, 0, 0, 9, 0],
-          [0, 0, 0, 0, 8, 5, 0, 0, 0]]
+---------- Data-related
+toBoard :: [[Int]] -> Board
+toBoard values = findEmpties $ Board { values = values, empty = fromList [],
+                                       size = len, ixs = [0..len - 1], blockSize = bs }
+  where bs = fromEnum . sqrt . toEnum $ length values
+        len = length values
 
--- [[0,7,1,4,0,8,0,3,5],[0,0,0,0,5,3,0,8,7],[0,0,3,9,0,7,6,0,0],[0,0,0,0,0,1,0,0,0],[0,9,0,8,0,6,0,0,3],[0,0,0,0,0,4,8,2,0],[0,6,0,3,4,9,7,0,8],[3,0,0,0,0,2,0,9,0],[0,0,0,0,8,5,3,0,0]]
+findEmpties :: Board -> Board
+findEmpties board = board { empty = fromList [(x, y) | y <- is, x <- is, blank (x, y)] }
+  where blank (x, y) = 0 == ((values board) !! y !! x)
+        is = ixs board
 
+sample = toBoard [[0,7,1,4,0,0,0,0,5],
+                  [0,0,0,0,5,0,0,8,0],
+                  [0,0,3,9,0,7,6,0,0],
+                  [0,0,0,0,0,1,0,0,0],
+                  [0,9,0,8,0,6,0,0,3],
+                  [0,0,0,0,0,0,8,2,0],
+                  [0,6,0,0,4,0,7,0,8],
+                  [3,0,0,0,0,0,0,9,0],
+                  [0,0,0,0,8,5,0,0,0]]
+
+---------- Utility
 mapply :: (a, b) -> [(a -> b -> c)] -> [c]
 mapply args fns = map (\fn -> uncurry fn $ args) fns
 
 intFloor :: Int -> Int -> Int
 intFloor a b = fromEnum . floor . toEnum $ a `div` b
 
----------- Prining
-printBoard :: Board -> IO ()
-printBoard board = printChunk board
-  where blockSize = fromEnum . sqrt . toEnum $ length board
-        delim = replicate (blockSize + (2 * length board)) '-'
-        ch = chunk blockSize
-        printChunk = ch printLine (putStrLn delim) (putStr "")
-        printLine = ch printSquare (putStr "|") (putStrLn "")
-        printSquare 0 = putStr "  "
-        printSquare num = putStr . (++ " ") $ show num
-
-chunk :: Int -> (a -> IO ()) -> IO () -> IO () -> [a] -> IO ()
-chunk chunkSize each delim finally corpus = rec corpus
-  where rec [] = finally
-        rec piece = do mapM_ each $ take chunkSize piece
-                       delim
-                       rec $ drop chunkSize piece
+thd :: (a, b, c) -> c
+thd (a, b, c) = c
